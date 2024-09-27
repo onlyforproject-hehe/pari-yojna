fd32e677efc5a6f24ce0074bfd8af32b7876470a
onlyforproject-hehe <shehbaz24mit@student.mes.ac.in>
2024-09-28 01:35:58 +0530

    Updated App.js to fix useEffect dependency issue

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    transform: {
-        '^.+\\.jsx?$': 'babel-jest', // This will use babel-jest to transform your .js and .jsx files
-    },
-    testEnvironment: 'jsdom', // This is important for React testing
-};
